3c31d190343ed648499c0f3c7cfa9c4be74444e5
unknown <akshatlambhate180302@gmail.com>
2025-01-29 18:09:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./Components/Hero"
+import Highlights from "./Components/Highlights"
+import Model from "./Components/Model"
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <main className="bg-black">
+    <Navbar />
+    <Hero />
+    <Highlights />
+    <Model />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/ModelView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModelView = ({index,groupRef,gsapType,controlRef,setRotationSize,size,item}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ModelVi
